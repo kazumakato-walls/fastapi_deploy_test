5e670c2e043bdf34294ac42bcb88ba83c6e2f61a
kazumakato-walls <107688753+kazumakato-walls@users.noreply.github.com>
2024-07-31 15:39:29 +0900

    DB update

--- db.py
+++ db.py
@@ -2,7 +2,7 @@
 # DBへの接続設定
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
